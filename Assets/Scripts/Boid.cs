9aa86ffaf094b5c41df21f51d4141d1cf806aa32
Joshua <joshuabetz12@gmail.com>
2020-11-23 22:43:09 -0500

    added boid spawner. corrected pathing algorithm.

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -5,9 +5,10 @@ using System.Linq;
 using UnityEngine;
 
 public class Boid : MonoBehaviour {
-    private Vector3 sceneOrigin = new Vector3(0, 8, 0);
+    private Vector3 sceneOrigin = new Vector3(0, 15, 0);
     private Settings settings;
-    [HideInInspector] public Vector3 velocity;
+    [HideInInspector]
+    public Vector3 velocity;
 
     private void Awake() {
         settings = FindObjectOfType<Settings>();
@@ -18,27 +19,28 @@ public class Boid : MonoBehaviour {
     }
 
     void Update() {
-        var neighbors = GetNeighboringBoids(settings.searchRadius);
+        var visibleNeighbors = GetNeighboringBoids(settings.searchRadius);
         var closeNeighbors = GetNeighboringBoids(settings.separationRadius);
-        if (neighbors.Length > 0) {
-            var averagePosition = GetAveragePosition(settings.searchRadius).normalized;
+        if (visibleNeighbors.Length > 0) {
+            
+            var averagePosition = GetAveragePosition(settings.searchRadius);
             var averageVelocity = GetAverageVelocity(settings.searchRadius).normalized;
-            var averageCloseByPosition = GetAveragePosition(settings.separationRadius).normalized;
+            var averageCloseByPosition = GetAveragePosition(settings.separationRadius);
             var separation = (transform.position - averageCloseByPosition).normalized;
-
+            
             velocity = Vector3.Lerp(velocity, SteerTowards(averagePosition), settings.cohesionWeight);
             velocity = Vector3.Lerp(velocity, SteerTowards(averageVelocity), settings.alignmentWeight);
             velocity = Vector3.Lerp(velocity, separation, settings.separationWeight * closeNeighbors.Length);
         }
-
+        
         var normalized = velocity.normalized;
         velocity = normalized * settings.maxSpeed;
         var ray = new Ray(transform.position, transform.forward);
         if (Physics.SphereCast(ray, 0.75f, out var hit, 2)) {
-            var t = (settings.collisionMultiplier * Time.deltaTime) / hit.distance;
-            velocity = Vector3.Lerp(velocity, (sceneOrigin - transform.position), t);
+            var t = settings.collisionMultiplier * Time.deltaTime / hit.distance;
+            velocity = Vector3.Lerp(velocity, sceneOrigin - transform.position, t);
         }
-
+        
         transform.position += velocity * Time.deltaTime;
         transform.forward = velocity / velocity.magnitude;
     }
@@ -67,5 +69,11 @@ public class Boid : MonoBehaviour {
             }
         }
         return neighbors.ToArray();
+    }
+
+    private Vector3[] GetRayCastingDirections() {
+        var directions = new Vector3[300];
+        
+        return directions;
     }
 }
---
+++ Assets/Scripts/BoidSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidSpawner : MonoBehaviour {
+    
+    public GameObject prefab;
+    public float maxDeviation;
+    public int amount;
+
+    void Start() {
+        for (var i = 0; i < amount; i++) {
+            var position = CreateRandomPosition();
+            SpawnBoid(position);
+        }
+    }
+
+    private float GetValueInRange(float origin) {
+        var min = origin - maxDeviation;
+        var max = origin + maxDeviation;
+        return Random.Range(min, max);
+    }
+    
+    private Vector3 CreateRandomPosition() {
+        var x = GetValueInRange(transform.position.x);
+        var y = GetValueInRange(transform.position.y);
+        var z = GetValueInRange(transform.position.z);
+        return new Vector3(x, y, z);
+    }
+
+    private void SpawnBoid(Vector3 position) {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
